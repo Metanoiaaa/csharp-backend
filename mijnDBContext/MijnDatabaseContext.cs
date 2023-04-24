0ac390a8c0f0130b40273745abcfd4a9783808f3
David van Brakel <davidhvanbrakel@outlook.com>
2023-04-24 15:28:25 +0200

    tweedecommitDavid

---
+++ mijnDBContext/MijnDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mijnDBContext
+{
+    public class MijnDatabaseContext : DbContext
+    {
+        public MijnDatabaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<Recept> recepten { get; set; }
+    }
+}
---
+++ mijnDBContext/Recept.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace mijnDBContext
+{
+    public class Recept
+    {
+        public int Id { get; set; }
+        public string ReceptName { get; set; }
+        public string GebruikersNaam {  get; set; }
+
+       // public List<Recept> Recepten {  get; set; } = new List<Recept>();
+
+    }
+}
