9eb4dc2dd0cd1ea090c3b1df129c33543c5b7739
FelixvL <digitaleservice@gmail.com>
2023-04-24 16:44:59 +0200

    gelukt

---
+++ csharp-backend/Controllers/IngredientenController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mijnDBContext;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace csharp_backend.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientenController : ControllerBase
+    {
+            private MijnDatabaseContext _db;
+            public IngredientenController(MijnDatabaseContext db) {
+                _db = db;
+            }
+            [HttpGet]
+            public IEnumerable<Recept> Get()
+            {
+                Recept huis = new Recept();
+                huis.ReceptName = "Vlakbij";
+               huis.GebruikersNaam = "doen";
+                _db.Add(huis);
+                _db.SaveChanges();
+                return _db.recepten;
+            }
+
+        // GET: api/<IngredientenController>
+
+
+        // GET api/<IngredientenController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<IngredientenController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<IngredientenController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<IngredientenController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
