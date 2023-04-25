40ee54335284cc7f3b78ed05764df6a371dac8a9
David van Brakel <davidhvanbrakel@outlook.com>
2023-04-25 14:01:35 +0200

    basis werkt nu

--- csharp-backend/Controllers/ReceptenController.cs
+++ csharp-backend/Controllers/ReceptenController.cs
@@ -19,42 +19,61 @@ namespace csharp_backend.Controllers
 
         // GET: api/<ReceptenController>
         [HttpGet]
-        public IEnumerable<Recept> Get()
+        public List<Recept> Get()
         {
-            Recept recept = new Recept();
-            recept.ReceptName = "Broodje bal";
-            recept.Gebruikersnaam = "David";
-            recept.Vegan = false;
-            recept.Rating = 5;
-            _mdc.Add(recept);
-            _mdc.SaveChanges();
-
-            return _mdc.recepten;
+            return this._mdc.recepten.ToList();
         }
 
         // GET api/<ReceptenController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Recept? Get(int id)
         {
-            return "value";
+            return this._mdc.recepten.Find(id);
         }
 
         // POST api/<ReceptenController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Recept r)
         {
+            this._mdc.recepten.Add(r);
+            this._mdc.SaveChanges();
         }
 
         // PUT api/<ReceptenController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public bool Put(int id, [FromBody] Recept newRecept)
         {
+            // openen
+            Recept dbRecept = this._mdc.recepten.Find(id);
+            if (dbRecept == null)
+                return false;
+
+            // aanpassen
+            dbRecept.ReceptName = newRecept.ReceptName;
+            dbRecept.Vegan = newRecept.Vegan;
+            dbRecept.Gebruikersnaam = newRecept.Gebruikersnaam;
+
+            // opslaan
+            this._mdc.recepten.Update(dbRecept);
+            this._mdc.SaveChanges();
+
+            return true;
         }
 
         // DELETE api/<ReceptenController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public bool Delete(int id)
         {
+            // eerst de ingredient zoeken om te checken of deze wel bestaat
+            Recept dbRecept = this._mdc.recepten.Find(id);
+            if (dbRecept == null)
+                return false;
+
+            // dan ingredient verwijderen
+            this._mdc.recepten.Remove(dbRecept);
+            this._mdc.SaveChanges();
+
+            return true;
         }
     }
 }
